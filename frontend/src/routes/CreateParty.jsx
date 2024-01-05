324a2ec586162a2b01d2e1664fef914bb20ca3be
Caio Bruno <caiobruno90@yahoo.com>
2024-01-05 08:23:54 -0300

    Criando pagina de inserção de festa

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,7 +1,39 @@
 
 const CreateParty = () => {
   return (
-    <div>CreateParty</div>
+    <div className="Form-page">
+      <h2>Crie a sua proxima Festa</h2>
+      <p>Defina o seu orçamento e escolha o serviço</p>
+      <form >
+        <label>
+          <span>Nome da festa:</span>
+          <input type="text" placeholder="Seja criativo..." required />
+        </label>
+        <label>
+          <span>Anfitrião:</span>
+          <input type="text" placeholder="Quem esta dando a festa?" required />
+        </label>
+        <label>
+          <span>Descrição:</span>
+          <textarea placeholder="Conte mais sobre a festa..." required />
+        </label>
+        <label>
+          <span>Orçamento;</span>
+          <input type="number" placeholder="Quanto voce pretende investir?" required />
+        </label>
+        <label >
+          <span>Image:</span>
+          <input type="text" placeholder="Insira URL de una inagen" required />
+        </label>
+        <div>
+          <h2>Escolha os serviços</h2>
+          <div className="servicos-containet">
+            <p>Serviços</p>
+          </div>
+        </div>
+        <input type="submit" value="Criar Festa" className="btn" />
+      </form>
+    </div>
   )
 }
 
